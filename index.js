25f4447ea140bb6bf708f30dee91696d84a6a0b6
wj-tan <60432453+wj-tan@users.noreply.github.com>
2022-06-03 22:48:09 +0800

    Added X & Y axis

--- index.js
+++ index.js
@@ -14,4 +14,25 @@ const svg = d3.select("#my_dataviz")
 // 2020 crime rate data
 d3.csv("2020_crimes.csv").then(function (data) {
     console.log(data)
+
+// X axis, types of crime
+const x = d3.scaleBand()
+  .range([ 0, width ])
+  .domain(data.map(d => d.level_2))
+  .padding(0.2);
+svg.append("g")
+  .attr("transform", `translate(0, ${height})`)
+  .call(d3.axisBottom(x))
+  .selectAll("text")
+    .attr("transform", "translate(-10,0)rotate(-45)")
+    .style("text-anchor", "end");
+
+
+// Y axis, 
+const y = d3.scaleLinear()
+  .domain([0, 20000])
+  .range([ height, 0]);
+svg.append("g")
+  .call(d3.axisLeft(y));
 })
+
